64b46253c9650b4b1f1c49baf121a33991c50058
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2023-09-01 19:14:59 -0300

    .

---
+++ Sprint3/src/entidadeDAO/MeiodofeedbackDAO.java
@@ -0,0 +1,12 @@
+package entidadeDAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MeiodofeedbackDAO {
+    
+}
