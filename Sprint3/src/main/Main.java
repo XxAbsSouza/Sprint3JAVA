1f39bba7d6c598148ba62ca4d4d959c50de371bb
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2023-09-01 22:51:40 -0300

    .

--- Sprint3/src/main/Main.java
+++ Sprint3/src/main/Main.java
@@ -1,6 +1,9 @@
 package main;
-public class App {
+
+import menu.Menu;
+
+public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new Menu().menu();
     }
 }
